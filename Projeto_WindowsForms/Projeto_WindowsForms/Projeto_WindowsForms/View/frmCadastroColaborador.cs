20487e499039e087c8a5b5444446b4ba70405931
Ewerton Shoiti Okada <105746487+ewerton139@users.noreply.github.com>
2023-09-06 21:02:12 -0300

    Tela de cadastro de colaboradores

---
+++ Projeto_WindowsForms/Projeto_WindowsForms/Projeto_WindowsForms/View/frmCadastroColaborador.cs
@@ -0,0 +1,15 @@
+namespace CadastroDeColaboradores
+{
+    public partial class frmCadastroColaborador : Form
+    {
+        public frmCadastroColaborador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cadastro com sucesso");
+        }
+    }
+}
