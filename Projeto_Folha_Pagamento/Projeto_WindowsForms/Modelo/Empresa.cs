a238586cf52ef296e440d927750105479385d2db
Ewerton Shoiti Okada <105746487+ewerton139@users.noreply.github.com>
2023-10-09 18:20:44 -0300

    Delete Projeto_Folha_Pagamento directory

--- Projeto_Folha_Pagamento/Projeto_WindowsForms/Modelo/Empresa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Projeto_WindowsForms.Modelo
-{
-    public class Empresa
-    {
-        public string Cnpj { get; set; }
-        public string RazaoSocial { get; set; }
-        public string NomeFantasia { get; set; }
-    }
-}
