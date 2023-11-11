6c6302d13dc8af9597277122dbfda058b9243f33
Felipe Carvalho <felipeemanuelc12@gmail.com>
2023-11-11 15:32:31 -0300

    Telas de relatorio

---
+++ Projeto_WindowsForms/Projeto_Web/Controllers/RelatorioController.cs
@@ -0,0 +1,67 @@
+﻿using Servico;
+using Projeto_Web.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Modelo.Enum;
+
+namespace Projeto_Web.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public IActionResult RelatorioColaborador(int id)
+        {
+            var colaboradorServico = new ColaboradorServico();
+
+            var colaborador = colaboradorServico.BuscarColaborador(id);
+            var listaColaborador = colaboradorServico.ListarColaborador();
+
+            // Caso o colaborador não seja adiministrador, só poderá ver o próprio relátório
+            if (colaborador.Cargo != TipoCargo.Administrador)
+                listaColaborador = listaColaborador.Where(x => x.Id == colaborador.Id).ToList();
+
+            var viewModel = new RelatorioViewModel()
+            {
+                Colaborador = colaborador,
+                Colaboradores = listaColaborador
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult RelatorioEmpresa(int id)
+        {
+            var colaboradorServico = new ColaboradorServico();
+            var empresaServico = new EmpresaServico();
+
+            var colaborador = colaboradorServico.BuscarColaborador(id);
+            var listaEmpresa = empresaServico.ListarEmpresa();
+
+            var viewModel = new RelatorioViewModel()
+            {
+                Colaborador = colaborador,
+                Empresas = listaEmpresa
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult RelatorioFolhaPagamento(int id)
+        {
+            var colaboradorServico = new ColaboradorServico();
+            var folhaPagametoServico = new FolhaPagamentoServico();
+
+            var colaborador = colaboradorServico.BuscarColaborador(id);
+            var listaFolhaPagamento = folhaPagametoServico.ListarFolhaPagamento();
+
+            if (colaborador.Cargo != TipoCargo.Administrador)
+                listaFolhaPagamento = listaFolhaPagamento.Where(x => x.Colaborador.Id == colaborador.Id).ToList();
+
+            var viewModel = new RelatorioViewModel()
+            {
+                Colaborador = colaborador,
+                FolhasPagamento = listaFolhaPagamento
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Projeto_WindowsForms/Projeto_Web/ViewModel/RelatorioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Modelo;
+
+namespace Projeto_Web.ViewModel
+{
+    public class RelatorioViewModel
+    {
+        public Colaborador Colaborador { get; set; }
+
+        public IList<Colaborador> Colaboradores { get; set; }
+
+        public IList<Empresa> Empresas { get; set; }
+
+        public IList<FolhaPagamento> FolhasPagamento { get; set; }
+    }
+}
